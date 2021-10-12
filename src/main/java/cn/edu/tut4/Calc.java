dc95af7f16f7681e8905430a17a41eff35e95c77
Kunkun <2769739806@qq.com>
2021-10-12 10:23:48 +0800

    First

---
+++ src/main/java/cn/edu/tut4/Calc.java
@@ -0,0 +1,19 @@
+package cn.edu.tut4;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calc
+{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+static int add(int a, int b){
+        return a+b;
+    }
+    int subtract(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/cn/edu/tut4/CalcTest.java
@@ -0,0 +1,15 @@
+package cn.edu.tut4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    void add() {
+        int a = 1;
+        int b = 2;
+        assertEquals(a+b,Calc.add(a,b));
+    }
+}
